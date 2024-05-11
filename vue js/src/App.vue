8f0edd6f4ad56cb62e9025bef5d6107e33811700
Achraf CHAIR <139182135+achrafdevl@users.noreply.github.com>
2024-05-11 15:23:04 +0100

    hero section

---
+++ vue js/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import hero from './components/Hero.vue'
+</script>
+
+<template>
+  <hero />
+</template>
---
+++ vue js/src/components/Hero.vue
@@ -0,0 +1,57 @@
+<script setup>
+  const links = [
+    { name: 'Home', url: "#", active: true },
+    { name: 'App', url: "#"},
+    { name: 'Feautures', url: "#"},
+    { name: 'Testimonials', url: "#"},
+
+  ]
+</script>
+
+<template>
+  
+  <header class="">
+    <nav class=" =container flex justify-between items-center">
+      <div class="flex justify-between gap-20">
+        <!-- logo -->
+
+        <a href="#">VueDesing</a>
+
+        <!-- navBar -->
+        <div  class=" flex items-center text-xl gap-2">
+
+          <a
+            :href="link.url"
+            class="px-4 py-3 bg-pink-50"
+            v-for="(link, index) in links"
+            :key="index"
+            :class="{'text-indigo-900 font-bold': link.active}"
+          >
+            {{ link.name }}
+          </a>
+
+          <a href="#">
+            About
+          </a>
+
+          <a href="#">
+            Contact
+          </a>
+
+        </div>
+
+      </div>
+      
+      <!--  -->
+      <a href="#">
+        Get Started
+      </a>
+      
+    </nav>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
