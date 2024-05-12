b8a1e0ac3dbe490057e008117aeeb53d42bed1e8
Achraf CHAIR <139182135+achrafdevl@users.noreply.github.com>
2024-05-12 04:33:15 +0100

    TestiSec

--- vue js/src/App.vue
+++ vue js/src/App.vue
@@ -2,10 +2,12 @@
 import hero from './components/HeroSect.vue'
 import ContentSec from './components/ContentSec.vue'
 import FeautSec from './components/FeautSec.vue'
+import TestiSec from './components/TestiSec.vue'
 </script>
 
 <template>
   <hero />
   <ContentSec />
   <FeautSec />
+  <TestiSec />
 </template>
---
+++ vue js/src/components/TestiSec.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="py-20 bg-slate-200/20">
+        <div class="container">
+            <div class="text-center space-y-10">
+                <p>Join tens of thousands of music lovers</p>
+                <h2 class="text-5xl">
+                    What people say
+                </h2>
+            </div>
+
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
+                <div class="flex flex-col items-center space-y-4">
+                    <img src="" alt="Testimonial 1" class="w-24 h-24 rounded-full"/>
+                    <p class="text-xl font-semibold">John Doe</p>
+                    <p class="text-sm text-gray-500">CEO at Company</p>
+                    <p class="font-bold">Amazing sound !</p>
+                    <p class="text-gray-600 text-center">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                        viverra ipsum dolor, ultricies fermentum massa consequat eu.
+                        Nullam nec elit eget dolor pretium semper vel vitae neque.
+                    </p>
+                </div>
+            </div>
+                    
+
+        </div>
+    </section>
+
+</template>
